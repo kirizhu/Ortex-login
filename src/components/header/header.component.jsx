97a431c234f6e219782f262d5702077d01eddd70
KiriZhu <kiribaty88@gmail.com>
2021-01-10 21:35:09 +0100

    opened websocket but not getting desired data yet

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Navbar } from 'react-bootstrap';
 import './header.styles.scss';
 
 const Header = () => {
+  const [data, setData] = useState('');
+
+  let socket = new WebSocket(
+    'ws://stream.tradingeconomics.com/?client=guest:guest'
+  );
+
+  socket.onmessage = function (e) {
+    setData(e.data);
+    console.log(e.data);
+  };
+
+  socket.onopen = function (e) {
+    socket.send('{"topic": "subscribe", "to": "EURUSD:CUR"}');
+  };
+
   return (
-    <Navbar className='my-nav' bg='light' variant='light'>
+    <Navbar className='my-nav' bg='primary' variant='dark'>
       <Navbar.Brand href='#home'>Ortex</Navbar.Brand>
+      <span>{data}</span>
     </Navbar>
   );
 };
